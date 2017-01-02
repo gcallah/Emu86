6b86894c602c407baf66412e378a405a8fa3e19e
gcallah <gcallah@mac.com>
2017-01-02 07:29:06 +0000

    Added registers.py.

---
+++ registers.py
@@ -0,0 +1,21 @@
+"""
+registers.py
+Keeps track of x86 registers.
+"""
+
+class Registers():
+    """
+    Eight 32-bit registers, like any good, modern x86.
+    """
+
+    def __init__(self, vals=None):
+        self.vals = {
+                'EAX': 0,
+                'EBX': 0,
+                'ECX': 0,
+                'EDX': 0,
+                'ESI': 0,
+                'EDI': 0,
+                'ESP': 0,
+                'EBP': 0,
+                }
