011a54bd781f6724cf2eaa5d6010397bbc1ae1a6
gcallah <gcallah@mac.com>
2017-01-11 17:26:45 +0000

    Added data_mov.py.

--- assemble.py
+++ assemble.py
@@ -5,8 +5,9 @@ Executes assembly code typed in.
 
 from .errors import *  # import * OK here:
                        # these are *our* errors, after all!
-from .arithmetic import add, sub, imult, idiv, inc, dec, shl, 
+from .arithmetic import add, sub, imul, idiv, inc, dec, shl
 from .arithmetic import shr, notf, andf, orf, xor
+from .data_mov import mov
 from .parse import get_token, get_op, get_one_op, get_two_ops
 
 
@@ -17,17 +18,9 @@ def add_debug(s):
     global debug
     debug += (s + "\n")
 
-def move(code, registers, memory, code_pos):
-    """
-    Implments the MOV instruction.
-    """
-    (op1, op2, code_pos) = get_two_ops("MOV", code, registers, memory, code_pos)
-    op1.set_val(op2.get_val())
-    return ('', code_pos)
-
 instructions = {
         # data movement:
-        'MOV': move,
+        'MOV': mov,
         # arithmetic and logic:
         'ADD': add,
         'IMUL': imul,
