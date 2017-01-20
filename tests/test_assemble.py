3a2bf7c809112febcda26ac45c15d227f4705dca
gcallah <gcallah@mac.com>
2017-01-20 21:48:45 +0000

    Cut over to new data scheme.

--- tests/test_assemble.py
+++ tests/test_assemble.py
@@ -8,45 +8,44 @@ sys.path.append("..")
 
 from unittest import TestCase, main
 
-from Emu86.global_data import registers, flags, memory
+from Emu86.global_data import gdata
 from Emu86.assemble import assemble
 
 
 class AssembleTestCase(TestCase):
 
     def test_mov(self):
-        assemble("mov eax, 1", registers, memory, flags)
-        self.assertEqual(registers["EAX"], 1)
+        assemble("mov eax, 1", gdata)
+        self.assertEqual(gdata.registers["EAX"], 1)
 
     def test_add(self):
-        registers["EAX"] = 2
-        registers["EBX"] = 4
-        assemble("add eax, ebx", registers, memory, flags)
-        self.assertEqual(registers["EAX"], 6)
+        gdata.registers["EAX"] = 2
+        gdata.registers["EBX"] = 4
+        assemble("add eax, ebx", gdata)
+        self.assertEqual(gdata.registers["EAX"], 6)
 
     def test_sub(self):
-        registers["EAX"] = 2
-        registers["EBX"] = 4
-        assemble("sub eax, ebx", registers, memory, flags)
-        self.assertEqual(registers["EAX"], -2)
+        gdata.registers["EAX"] = 2
+        gdata.registers["EBX"] = 4
+        assemble("sub eax, ebx", gdata)
+        self.assertEqual(gdata.registers["EAX"], -2)
 
     def test_imul(self):
-        registers["EAX"] = 2
-        registers["EBX"] = 4
-        assemble("imul eax, ebx", registers, memory, flags)
-        self.assertEqual(registers["EAX"], 8)
+        gdata.registers["EAX"] = 2
+        gdata.registers["EBX"] = 4
+        assemble("imul eax, ebx", gdata)
+        self.assertEqual(gdata.registers["EAX"], 8)
 
     def test_and(self):
-        registers["EAX"] = 2
-        registers["EBX"] = 3
-        assemble("and eax, ebx", registers, memory, flags)
-        self.assertEqual(registers["EAX"], 2)
+        gdata.registers["EAX"] = 27
+        assemble("and eax, 23", gdata)
+        self.assertEqual(gdata.registers["EAX"], 19)
 
     def test_or(self):
-        registers["EAX"] = 2
-        registers["EBX"] = 3
-        assemble("or eax, ebx", registers, memory, flags)
-        self.assertEqual(registers["EAX"], 3)
+        gdata.registers["EAX"] = 2
+        gdata.registers["EBX"] = 3
+        assemble("or eax, ebx", gdata)
+        self.assertEqual(gdata.registers["EAX"], 3)
         
 if __name__ == '__main__':
     main()
