25ddc2ed3c38f2e75689f46773e9fcbac9c78276
NathanConroy <nathanconroydev@gmail.com>
2017-01-21 01:45:44 +0000

    Nathan added tests for xor, shl, shr, and neg.

--- tests/test_assemble.py
+++ tests/test_assemble.py
@@ -37,15 +37,34 @@ class AssembleTestCase(TestCase):
         self.assertEqual(gdata.registers["EAX"], 8)
 
     def test_and(self):
-        gdata.registers["EAX"] = 27
-        assemble("and eax, 23", gdata)
-        self.assertEqual(gdata.registers["EAX"], 19)
+        gdata.registers["EAX"] = 27                     #...011011
+        assemble("and eax, 23", gdata)                  #...001111
+        self.assertEqual(gdata.registers["EAX"], 19)    #...001011
 
     def test_or(self):
-        gdata.registers["EAX"] = 2
-        gdata.registers["EBX"] = 3
-        assemble("or eax, ebx", gdata)
-        self.assertEqual(gdata.registers["EAX"], 3)
-        
+        gdata.registers["EAX"] = 18                      #...010010
+        assemble("or eax, 24", gdata)                    #...011000
+        self.assertEqual(gdata.registers["EAX"], 26)     #...011010
+
+    def test_xor(self):
+        gdata.registers["EAX"] = 18                      #...010010
+        assemble("xor eax, 24", gdata)                   #...011000
+        self.assertEqual(gdata.registers["EAX"], 10)     #...001010
+
+    def test_shl(self):
+        gdata.registers["EAX"] = 18                      #...010010
+        assemble("shl eax, 2", gdata)
+        self.assertEqual(gdata.registers["EAX"], 72)     #..1001000
+
+    def test_shr(self):
+        gdata.registers["EAX"] = 18                      #...010010
+        assemble("shr eax, 2", gdata)
+        self.assertEqual(gdata.registers["EAX"], 4)      #...000100
+
+    def test_neg(self):
+        gdata.registers["EAX"] = 18
+        assemble("neg eax", gdata)
+        self.assertEqual(gdata.registers["EAX"], -18)
+
 if __name__ == '__main__':
     main()
