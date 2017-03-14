2b46f0ae3ccfc96626004dd179ade4efe4cf3b49
gcallah <gcallah@mac.com>
2017-03-14 18:17:07 -0400

    Trying to fix end-of-line comment problem.

--- assembler/errors.py
+++ assembler/errors.py
@@ -18,10 +18,15 @@ class InvalidOperand(Error):
         self.msg = "Invalid operand: " + offender
 
 class InvalidNumArgs(Error):
-    def __init__(self, offender, correct_num, actual_num):
+    def __init__(self, offender, correct_num, actual_num,
+                 extra_arg=None):
+        extra = ""
+        if extra_arg is not None:
+            extra = "; possible extra = " + extra_arg
         self.msg = ("Invalid number of args: " + offender
                     + " requires " + str(correct_num)
-                    + " but we got " + str(actual_num))
+                    + " but we got " + str(actual_num)
+                    + extra)
 
 class InvalidMemLoc(Error):
     def __init__(self, offender):
@@ -35,5 +40,10 @@ def check_num_args(instr, ops, correct_num):
     """
     See if we have the proper number of arguments.
     """
-    if len(ops) != correct_num:
-        raise InvalidNumArgs(instr, correct_num, len(ops))
+    l = len(ops)
+    if l != correct_num:
+        extra_arg = None
+        if l > correct_num:
+            extra_arg = ops[l - 1]
+        raise InvalidNumArgs(instr, correct_num, len(ops),
+                             extra_arg)
