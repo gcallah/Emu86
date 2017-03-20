f1d20a2451c9ce6495f2b1585863ed59198cd1fb
gcallah <gcallah@mac.com>
2017-03-20 01:02:29 -0400

    Building development.

--- assembler/interrupts.py
+++ assembler/interrupts.py
@@ -9,7 +9,7 @@ from .global_data import gdata
 def read_key(gdata):
     # we are faking 'reading' from the keyboard
     c = gdata.ret_str[gdata.nxt_key]
-    gdata.nxt_key = (gdata.nxt_key + 1) % len(ret_str)
+    gdata.nxt_key = (gdata.nxt_key + 1) % len(gdata.ret_str)
     gdata.registers['EAX'] = ord(c)
     return ""
 
