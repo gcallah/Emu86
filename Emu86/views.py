61650fb7d01b241303f44bd309da0873baf2a5e5
gcallah <gcallah@mac.com>
2017-03-25 22:14:39 -0400

    Building development.

--- Emu86/views.py
+++ Emu86/views.py
@@ -54,7 +54,7 @@ def main_page(request):
             step = (STEP in request.POST)
             gdata.nxt_key = 0
             if step:
-                gdata.nxt_key = request.POST.get(NXT_KEY, 0)
+                gdata.nxt_key = int(request.POST.get(NXT_KEY, 0))
 
             get_reg_contents(gdata.registers, request)
             get_mem_contents(gdata.memory, request)
