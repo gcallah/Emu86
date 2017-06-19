9e303148dc07e7d38a21c7f310388cbb9e639674
Varun Chopra <vc1202@nyu.edu>
2017-06-19 16:49:46 +0530

    Save feature with Information hover of buttons

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "mysite","static"),
+]
+
+
 
 
 # Quick-start development settings - unsuitable for production
