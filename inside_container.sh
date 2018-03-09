9bcca25ad77886b3f851943f44d80364efa3e309
gcallah <gcallah@mac.com>
2018-03-09 14:26:03 -0500

    Updated comment

--- inside_container.sh
+++ inside_container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# run this from inside your container.
+# Run this from inside your container to launch Emu86.
 # Although *inside* the container the web server runs at 0.0.0.0:8000,
 # from outside it appears at 127.0.0.1:8000.
 
