ffeb4d1db54400d2969ccc835236dcf8e676bccd
gcallah <gcallah@mac.com>
2018-03-10 19:52:31 -0500

    Had wrong port number for Django server.

--- inside_container.sh
+++ inside_container.sh
@@ -4,4 +4,4 @@
 # Although *inside* the container the web server runs at 0.0.0.0:8000,
 # from outside it appears at 127.0.0.1:8000.
 
-./manage.py runserver 0.0.0.0:127
+./manage.py runserver 0.0.0.0:8000
