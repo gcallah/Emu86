340f91a0bc6d459be875c4a049282e72900895f3
gcallah <gcallah@mac.com>
2018-03-12 17:06:47 -0400

    Check for command line argument.

--- emu_container.sh
+++ emu_container.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [ -z "$1" ]
+  then
+    echo "You must provide the location of your Emu86 repo."
+    exit 1
+fi
 docker run -it -p 8000:8000 -v $1:/home/Emu86 --name emu86 gcallah/emu86:v4 bash
