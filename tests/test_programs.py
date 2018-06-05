79418a9935c84b1bfb28d859e221ebc5aeaa7b33
cindylee0113 <cindylee0113@gmail.com>
2018-06-05 16:47:43 -0400

    Created array testing

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -25,31 +25,28 @@ class TestPrograms(TestCase):
         with open (filenm, "r") as prog:
             return prog.read()
 
-    def test_loop(self):
+    def run_test_code (self, filnm):
         vmachine.re_init()
-        test_code = self.read_test_code("tests/loop.asm")
+        test_code = self.read_test_code(filnm)
         assemble(test_code, vmachine)
+
+    def test_loop(self):
+        self.run_test_code("tests/loop.asm")
         self.assertEqual(vmachine.registers["ECX"], 16)
 
     def test_power(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/power.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/power.asm")
         self.assertEqual(vmachine.registers["EDX"], 65536)
 
     def test_gt(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/gt.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/gt.asm")
         self.assertEqual(vmachine.registers["EAX"], 17)
         self.assertEqual(vmachine.registers["EBX"], 16)
         self.assertEqual(vmachine.registers["ECX"], 16)
         self.assertEqual(vmachine.registers["EDX"], 19)
 
     def test_interrupt(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/test_interrupt.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/test_interrupt.asm")
         self.assertEqual(vmachine.registers["EAX"], 71)
         self.assertEqual(vmachine.registers["EBX"], 6)
         self.assertEqual(vmachine.registers["ECX"], 1)
@@ -57,9 +54,7 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["6"], 71)
 
     def test_key(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/key_test.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/key_test.asm")
         self.assertEqual(vmachine.registers["EAX"], 71)
         self.assertEqual(vmachine.registers["EBX"], 71)
         self.assertEqual(vmachine.registers["ECX"], 1)
@@ -67,9 +62,7 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["9"], 83)
 
     def test_arithmetic_shift(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/arithmetic_shift.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/arithmetic_shift.asm")
         self.assertEqual(vmachine.registers["EAX"], 4)
         self.assertEqual(vmachine.registers["EBX"], 10)
         self.assertEqual(vmachine.registers["ECX"], 8)
@@ -77,18 +70,21 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["4"], 4)
 
     def test_jump(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/test_jump.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/test_jump.asm")
         self.assertEqual(vmachine.registers["EAX"], 3)
 
     def test_data(self):
-        vmachine.re_init()
-        test_code = self.read_test_code("tests/data.asm")
-        assemble(test_code, vmachine)
+        self.run_test_code("tests/data.asm")
         self.assertEqual(vmachine.registers["EAX"],  8)
         self.assertEqual(vmachine.registers["EBX"], 16)
         self.assertEqual(vmachine.registers["ECX"], 32)
-        
+
+    def test_array(self):
+        self.run_test_code("tests/array.asm")
+        self.assertEqual(vmachine.registers["EAX"],  3)
+        self.assertEqual(vmachine.registers["EBX"], 50)
+        self.assertEqual(vmachine.registers["ECX"], ord ('l'))
+        self.assertEqual(vmachine.registers["EDX"], 5)
+
 if __name__ == '__main__':
     main()
