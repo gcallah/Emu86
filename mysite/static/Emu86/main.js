228e57431fe71f0014067c30f05a152eaf34f145
cindylee0113 <cindylee0113@gmail.com>
2018-06-21 10:52:26 -0400

    Error alerts added to load page

--- mysite/static/Emu86/main.js
+++ mysite/static/Emu86/main.js
@@ -2,62 +2,67 @@
  * Created by Varun on 19/06/17.
  */
 
+function AlertError()
+{
+    var raised=document.getElementById("error")
+
+    if(raised)
+    {
+        raised=raised.value;
+        if (raised != "") 
+        {
+            alert(document.getElementById("error").value);
+        }
+    }
+}
+
+function loadcode()
+{
+    if(sessionStorage.loadonce)
+    {
+        AlertError();
+        return;
+    }
+    else
+    {
+        sessionStorage.loadonce=1;
+        code = localStorage.Code;
+        if(code!=undefined || code!=null) {
+            document.getElementById("id_code").value = code;
+        }
+    }
+}
 
 
-             function loadcode()
-                {
-                    if(sessionStorage.loadonce)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        sessionStorage.loadonce=1;
-                        code = localStorage.Code;
-                        if(code!=undefined || code!=null) {
-                            document.getElementById("id_code").value = code;
-                        }
-                    }
-                }
+function Savecode()
+{
+    if(confirm("Do you want to save this code?")) {
+        data = document.getElementById("id_code").value;
+        try {
+            localStorage.Code=data;
+            alert("Your code was saved successfully.");
+        }
+        catch(exception)
+        {
+            alert("An unknown error occured, please try again.");
 
+        }
+    }
+}
 
-                function Savecode()
-                {
-                    if(confirm("Do you want to save this code?")) {
-                        data = document.getElementById("id_code").value;
-                        try {
-                            localStorage.Code=data;
-                            alert("Your code was saved successfully.");
-                        }
-                        catch(exception)
-                        {
-                            alert("An unknown error occured, please try again.");
+function convert(name,value)
+{
+    var message= "Binary value of " + " " + name + ": ";
+    var value1=parseInt(value)
+    if(value1>=0) {
 
-                        }
-                    }
-                }
-
-                var raised=document.getElementById("error")
-
-                if(raised)
-                {
-                    raised=raised.value;
-                    alert(raised);
-                }
-
-                function convert(name,value)
-                {
-                    var message= "Binary value of " + " " + name + ": ";
-                    var value1=parseInt(value)
-                    if(value1>=0) {
-
-                        message=message+((value1).toString(2));
-                        alert(message);
-                    }
-                    else
-                    {
-                        message=message+((value1>>>0).toString(2));
-                        alert(message);
-                    }
-                }
+        message=message+((value1).toString(2));
+        alert(message);
+    }
+    else
+    {
+        message=message+((value1>>>0).toString(2));
+        alert(message);
+    }
+}
 
