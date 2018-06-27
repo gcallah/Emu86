716dd5f855a09db6b1bef3f64d093cb3b595cda6
gcallah <gcallah@mac.com>
2018-06-27 15:23:16 -0400

    Added new dirs for Intel and ATT

--- all_tests.sh
+++ all_tests.sh
@@ -5,6 +5,9 @@
 set -e
 
 export TDIR="tests"
+export ATT_DIR="$TDIR/ATT"
+export INTEL_DIR="$TDIR/Intel"
+echo "$INTEL_DIR"
 $TDIR/test_assemble.py
 $TDIR/test_errors.py
 $TDIR/test_control_flow.py
