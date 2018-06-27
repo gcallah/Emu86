8306b9d0c772fefc38db9ce04871423f50ee4af5
cindylee0113 <cindylee0113@gmail.com>
2018-06-27 16:25:11 -0400

    Edited to include Intel folder

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -31,22 +31,22 @@ class TestPrograms(TestCase):
         assemble(test_code, 'intel', vmachine)
 
     def test_loop(self):
-        self.run_test_code("tests/loop.asm")
+        self.run_test_code("tests/Intel/loop.asm")
         self.assertEqual(vmachine.registers["ECX"], 16)
 
     def test_power(self):
-        self.run_test_code("tests/power.asm")
+        self.run_test_code("tests/Intel/power.asm")
         self.assertEqual(vmachine.registers["EDX"], 65536)
 
     def test_gt(self):
-        self.run_test_code("tests/gt.asm")
+        self.run_test_code("tests/Intel/gt.asm")
         self.assertEqual(vmachine.registers["EAX"], 17)
         self.assertEqual(vmachine.registers["EBX"], 16)
         self.assertEqual(vmachine.registers["ECX"], 16)
         self.assertEqual(vmachine.registers["EDX"], 19)
 
     def test_interrupt(self):
-        self.run_test_code("tests/test_interrupt.asm")
+        self.run_test_code("tests/Intel/test_interrupt.asm")
         self.assertEqual(vmachine.registers["EAX"], 71)
         self.assertEqual(vmachine.registers["EBX"], 6)
         self.assertEqual(vmachine.registers["ECX"], 1)
@@ -54,7 +54,7 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["6"], 71)
 
     def test_key(self):
-        self.run_test_code("tests/key_test.asm")
+        self.run_test_code("tests/Intel/key_test.asm")
         self.assertEqual(vmachine.registers["EAX"], 71)
         self.assertEqual(vmachine.registers["EBX"], 71)
         self.assertEqual(vmachine.registers["ECX"], 1)
@@ -62,7 +62,7 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["9"], 83)
 
     def test_arithmetic_shift(self):
-        self.run_test_code("tests/arithmetic_shift.asm")
+        self.run_test_code("tests/Intel/arithmetic_shift.asm")
         self.assertEqual(vmachine.registers["EAX"], 4)
         self.assertEqual(vmachine.registers["EBX"], 10)
         self.assertEqual(vmachine.registers["ECX"], 8)
@@ -70,17 +70,17 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["4"], 4)
 
     def test_jump(self):
-        self.run_test_code("tests/test_jump.asm")
+        self.run_test_code("tests/Intel/test_jump.asm")
         self.assertEqual(vmachine.registers["EAX"], 3)
 
     def test_data(self):
-        self.run_test_code("tests/data.asm")
+        self.run_test_code("tests/Intel/data.asm")
         self.assertEqual(vmachine.registers["EAX"],  8)
         self.assertEqual(vmachine.registers["EBX"], 16)
         self.assertEqual(vmachine.registers["ECX"], 32)
 
     def test_array(self):
-        self.run_test_code("tests/array.asm")
+        self.run_test_code("tests/Intel/array.asm")
         self.assertEqual(vmachine.registers["EAX"],  3)
         self.assertEqual(vmachine.registers["EBX"], -50)
         self.assertEqual(vmachine.registers["ECX"], ord ('l'))
@@ -99,32 +99,32 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory[hex(22).split('x')[-1]], 0)
 
     def test_sum_calculation(self):
-        self.run_test_code("tests/sum_test.asm")
+        self.run_test_code("tests/Intel/sum_test.asm")
         self.assertEqual(vmachine.registers["EAX"],  53)
         self.assertEqual(vmachine.memory["1"], 53)
 
     def test_arithmetic_expression(self):
-        self.run_test_code("tests/arithmetic_expression.asm")
+        self.run_test_code("tests/Intel/arithmetic_expression.asm")
         self.assertEqual(vmachine.registers["EAX"],  -31)
         self.assertEqual(vmachine.registers["EBX"],  52)
 
     def test_area(self):
-        self.run_test_code("tests/area.asm")
+        self.run_test_code("tests/Intel/area.asm")
         self.assertEqual(vmachine.registers["EAX"],  35 * 27)
 
     def test_celsius_conversion(self):
-        self.run_test_code("tests/cel_to_fah.asm")
+        self.run_test_code("tests/Intel/cel_to_fah.asm")
         self.assertEqual(vmachine.registers["EAX"], 95)
         self.assertEqual(vmachine.registers["EDX"], 2)
         self.assertEqual(vmachine.registers["EBX"], 5)
 
     def test_log(self):
-        self.run_test_code("tests/log.asm")
+        self.run_test_code("tests/Intel/log.asm")
         self.assertEqual(vmachine.registers["EAX"], 1024)
         self.assertEqual(vmachine.registers["ECX"], 9)
 
     def test_mem_register(self):
-        self.run_test_code("tests/mem_register_test.asm")
+        self.run_test_code("tests/Intel/mem_register_test.asm")
         self.assertEqual(vmachine.registers["EAX"], 6)
         self.assertEqual(vmachine.memory["6"], 3)
         self.assertEqual(vmachine.memory["8"], 8)
@@ -132,18 +132,18 @@ class TestPrograms(TestCase):
         self.assertEqual(vmachine.memory["1"], 32)
 
     def test_array_avg(self):
-        self.run_test_code("tests/array_average_test.asm")
+        self.run_test_code("tests/Intel/array_average_test.asm")
         self.assertEqual(vmachine.registers["EAX"], 9)
         self.assertEqual(vmachine.registers["ECX"], 100)
         self.assertEqual(vmachine.registers["EBX"], 100)
         self.assertEqual(vmachine.registers["EDX"], 89)
 
     def test_int_square_root(self):
-        self.run_test_code("tests/int_square_root.asm")
+        self.run_test_code("tests/Intel/int_square_root.asm")
         self.assertEqual(vmachine.registers["EAX"], 10)
 
     def test_add_to_array_elem_test(self):
-        self.run_test_code("tests/change_array_elem_test.asm")
+        self.run_test_code("tests/Intel/change_array_elem_test.asm")
         self.assertEqual(vmachine.registers["EAX"], 3331)
         self.assertEqual(vmachine.registers["EDX"], 3331)
         self.assertEqual(vmachine.registers["ECX"], 100)
