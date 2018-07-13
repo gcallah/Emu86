d62f8e6ed431bb46d94f8c2fc2951fa4387f6852
cindylee0113 <cindylee0113@gmail.com>
2018-07-13 15:56:34 -0400

    Altered register order for MIPS and adjusted hightlighting code

--- mysite/static/Emu86/main.js
+++ mysite/static/Emu86/main.js
@@ -22,15 +22,53 @@ function loadcode()
     {
         AlertError();
         var instr = document.getElementsByName("last_instr")[0];
-        if (instr) {
+        var mips_ip = document.getElementsByName("PC");
+        var intel_ip = document.getElementsByName("EIP");
+        var ip_val = null;
+        if (mips_ip.length != 0){
+            ip_val = parseInt(mips_ip[0].value)/4;
+        }
+        else if (intel_ip.length != 0) {
+            ip_val = parseInt(intel_ip[0].value);
+        }
+        if (instr && ip_val) {
             var input = document.getElementById("id_code");
-            input.focus();
-            if (instr.value != "" && input.value.indexOf(instr.value) != -1) {
-                input.setSelectionRange(input.value.indexOf(instr.value), 
-                       input.value.indexOf(instr.value) + instr.value.length);
+            var countCode = 0;
+            var countRepeats = 0;
+            var codeArray = input.value.split("\n");
+            var textArea = true; 
+            if (instr.value != "") {
+                for (var index = 0; index < codeArray.length; index++) {
+                    var string = codeArray[index].trim();
+                    if (countCode == ip_val){
+                        break;
+                    }
+                    if (string == ".data"){
+                        textArea = false;
+                        continue;
+                    }
+                    else if (string == ".text"){
+                        textArea = true;
+                        continue;
+                    }
+                    if (!(string === "") && textArea && string[0] != ";"){
+                        countCode++; 
+                    }
+                    if (string == instr.value){
+                        countRepeats++;
+                    }
+                }
+                input.focus();
+                var startIndex = 0;
+                for (var time = 0; time < countRepeats; time++) {
+                    startIndex = input.value.indexOf(instr.value, startIndex) 
+                                 + instr.value.length;
+                }
+                startIndex -= instr.value.length;
+                input.setSelectionRange(startIndex, 
+                       startIndex + instr.value.length);
             }
         }
-        
         return;
     }
     else
