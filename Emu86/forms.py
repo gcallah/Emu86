00255105b01b69b82548bea1a14a8dd313a84a27
cindylee0113 <cindylee0113@gmail.com>
2018-07-19 14:32:55 -0400

    temp reset

--- Emu86/forms.py
+++ Emu86/forms.py
@@ -2,8 +2,13 @@ from django import forms
 
 
 class MainForm(forms.Form):
+    # code = forms.CharField(required = False, 
+    # 	                   widget=forms.Textarea(attrs={
+    #                                         'cols': 50,
+    #                                         'rows': 24, 
+    #                                         'oninput': 'checkForScript()'}))
+
     code = forms.CharField(required = False, 
     	                   widget=forms.Textarea(attrs={
                                             'cols': 50,
-                                            'rows': 24, 
-                                            'oninput': 'checkForScript()'}))
+                                            'rows': 24 }))
