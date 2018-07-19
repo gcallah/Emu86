11b61f576393a2dbb718332ff308b5fec7b4a1e6
cindylee0113 <cindylee0113@gmail.com>
2018-07-19 14:36:40 -0400

    Reverting back to not have script injection

--- Emu86/forms.py
+++ Emu86/forms.py
@@ -2,13 +2,8 @@ from django import forms
 
 
 class MainForm(forms.Form):
-    # code = forms.CharField(required = False, 
-    # 	                   widget=forms.Textarea(attrs={
-    #                                         'cols': 50,
-    #                                         'rows': 24, 
-    #                                         'oninput': 'checkForScript()'}))
-
     code = forms.CharField(required = False, 
     	                   widget=forms.Textarea(attrs={
                                             'cols': 50,
-                                            'rows': 24 }))
+                                            'rows': 24, 
+                                            'oninput': 'checkForScript()'}))
