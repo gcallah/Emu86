9e10ada13d1ce894b3cefe5cbc05e3c1cacd7d46
gcallah <gcallah@mac.com>
2018-08-28 18:13:57 -0400

    Updated run_local.sh to remove -it.

--- run_local.sh
+++ run_local.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker rm emu86 || true
-docker run -it -p 8000:8000 -v /tmp/Haldun:/home/Emu86 --name emu86 gcallah/emu86:v4 /home/Emu86/manage.py runserver 0.0.0.0:8000
+# we shouldn't need -it here:
+docker run -p 8000:8000 -v /tmp/Haldun:/home/Emu86 --name emu86 gcallah/emu86:v4 /home/Emu86/manage.py runserver 0.0.0.0:8000
