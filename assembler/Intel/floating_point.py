976511a85a5389fef82a26ec4be87e54b36280f0
Nicholas Pena <np1514@nyu.edu>
2018-09-21 17:42:16 -0400

    floating_point.py for intel assembler

---
+++ assembler/Intel/floating_point.py
@@ -0,0 +1,11 @@
+"""
+floating_point.py: arithmetic and logic instructions.
+"""
+
+import operator as opfunc
+
+from assembler.errors import *
+from assembler.tokens import Instruction, MAX_INT, Register, IntegerTok
+from assembler.ops_check import one_op_arith
+from .argument_check import * 
+
