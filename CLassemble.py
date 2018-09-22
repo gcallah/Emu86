b05c74a555717cae6adaf5dca6546fba044cbc2f
cindylee0113 <cindylee0113@gmail.com>
2018-09-22 12:32:27 -0400

    Editing command line layout for registers and memory

--- CLassemble.py
+++ CLassemble.py
@@ -13,6 +13,33 @@ numsys = {
     "2": "hex"
 }
 
+def convert_reg_contents(registers):
+    for reg in registers:
+        hex_list = hex(int(registers[reg])).split('x')
+        hex_list[1] = hex_list[1].upper()
+        if "-" in hex_list[0]:
+            registers[reg] = "-" + hex_list[1]
+        else:
+            registers[reg] = hex_list[1]
+
+def convert_mem_contents(memory):
+    for loc in memory:
+        hex_list = hex(int(memory[loc])).split('x')
+        hex_list[1] = hex_list[1].upper()
+        if "-" in hex_list[0]:
+            memory[loc] = "-" + hex_list[1]
+        else:
+            memory[loc] = hex_list[1]
+
+def convert_stack_contents(stack):
+    for loc in stack:
+        hex_list = hex(int(stack[loc])).split('x')
+        hex_list[1] = hex_list[1].upper()
+        if "-" in hex_list[0]:
+            stack[loc] = "-" + hex_list[1]
+        else:
+            stack[loc] = hex_list[1]
+
 def select_flavor():
     print("Welcome to Emu's command line assembler.")
     print("Please select your flavor:")
@@ -52,12 +79,31 @@ def obtain_file():
                 print("File not found! Try again!")
 
 def display_results(last_instr, error, vm):
+    if vm.base == "hex":
+        convert_stack_contents(vm.stack)
+        convert_mem_contents(vm.memory)
+        convert_reg_contents(vm.registers)
     print("Last instruction: ", last_instr)
     print("Error: ", error)
-    for reg in vm.registers:
-        print(reg, vm.registers[reg])
+    print("\nRegisters:")
+    if vm.flavor == "intel" or vm.flavor == "att":
+        for reg in vm.registers:
+            print("\t" + reg + ": " + str(vm.registers[reg]) + "\t")
+    else:
+        count = 0
+        for reg in vm.registers :
+            print("\t" + reg + ":" + str(vm.registers[reg]) + "\t", end = "")
+            if reg == "R20":
+                print()
+                count += 2
+            else:
+                count += 1
+                if count % 3 == 0:
+                    print()
+    print()
+    print("\nMemory: ")
     for mem in vm.memory:
-        print(mem, vm.memory[mem])
+        print("\t" + mem + ": " + str(vm.memory[mem]))
 
 
 def main():
@@ -74,7 +120,7 @@ def main():
     asm_file = open(file_nm, "r")
     code = ""
     for line in asm_file:
-        code += repr(line)[1:-1]
+        code += line
     print (code)
     if flavor == "intel" or flavor == "att":
         intel_machine.flavor = flavor
