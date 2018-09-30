3ffb5abeb05427a67659841ec8a893e7824f279a
Amit Biswas <ab4309@nyu.edu>
2018-09-30 00:20:50 -0400

    RISCV Sum test assemby code
    Register usage may not be fully correct. Will be fixed once functional

---
+++ tests/RISCV/sum_test.asm
@@ -0,0 +1,12 @@
+; Declare number and sum.
+.data
+    number: .word -0x69
+    sum: .word 0
+
+; Store first number to X8
+; Add 158 to value in X8
+; Store total to sum
+.text
+    0x40000 LW X8, 0(X28)		
+    0x40004 ADDI X8, X8, 0x9E	
+    0x40008 SW X8, 4(R28)
