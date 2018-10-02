cbbb0013b36a6f720bff5f27c19f5a8f29d8bd1c
LuvneeshM <lm3300@nyu.edu>
2018-10-02 12:25:34 -0400

    added an assembly program to test floating point multipication by calculating the area of a rectangle (pair programming with Nick Pena)

---
+++ tests/MIPS_ASM/fp_area.asm
@@ -0,0 +1,11 @@
+; Declare length and width
+.data
+	long: .float 12.2
+	wide: .float 12.5
+
+; Calc area of rect
+.text
+	0x40000 LWC F8, 0(F28)
+    0x40004 LWC F9, 4(F28)
+    0x40008 MULTS F8, F9
+    0x4000C MFLO F10
