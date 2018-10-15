0f6c23dfc8ed749040013829094c87d75203bbc6
LuvneeshM <lm3300@nyu.edu>
2018-10-15 01:32:36 -0400

    re-uncommenting out all the other tests in MIPS_ASM\test_programs.py

--- tests/MIPS_ASM/test_programs.py
+++ tests/MIPS_ASM/test_programs.py
@@ -205,24 +205,24 @@ class TestPrograms(TestCase):
         self.assertEqual(mips_machine.registers["F10"], 20.555)
 
     # power function
-    # def test_fp_power(self):
-    #     self.run_mips_test_code("fp_power.asm")
-    #     self.assertEqual(mips_machine.registers["F8"], 166.375)
+    def test_fp_power(self):
+        self.run_mips_test_code("fp_power.asm")
+        self.assertEqual(mips_machine.registers["F8"], 166.375)
 
-    #area function
-    # def test_fp_area(self):
-    #     self.run_mips_test_code("fp_area.asm")
-    #     # a = 12.2
-    #     # b = 12.5
-    #     # ah = self.float_to_hex(a)
-    #     # bh = self.float_to_hex(b)
-    #     # self.assertEqual(mips_machine.registers["F8"], a)
-    #     # self.assertEqual(mips_machine.registers["F9"], b)
+    # area function
+    def test_fp_area(self):
+        self.run_mips_test_code("fp_area.asm")
+        # a = 12.2
+        # b = 12.5
+        # ah = self.float_to_hex(a)
+        # bh = self.float_to_hex(b)
+        # self.assertEqual(mips_machine.registers["F8"], a)
+        # self.assertEqual(mips_machine.registers["F9"], b)
 
-    #     # result = self.convertHiLoForFP()
+        # result = self.convertHiLoForFP()
 
-    #     # correct = a*b
-    #     self.assertEqual(mips_machine.registers["F10"], 12.2 * 12.5)
+        # correct = a*b
+        self.assertEqual(mips_machine.registers["F10"], 12.2 * 12.5)
 
 
 
