8b192d27782888aa7968382176f2c8cd1ae28c03
Amit Biswas <ab4309@nyu.edu>
2018-10-27 21:32:23 -0400

    adding jalr skeleton code - riscv
    needs to be implemented. skeleton for now

--- assembler/RISCV/control_flow.py
+++ assembler/RISCV/control_flow.py
@@ -42,3 +42,20 @@ class Jal(Instruction):
     def fhook(self, ops, vm):
         target = get_one_op(self.get_nm(), ops)
         raise Jump(str(target.get_val()))
+
+class Jalr(Instruction):
+    """
+        <instr>
+            JALR
+        </instr>
+        <syntax>
+            JALR rd, rs1, imm
+        </syntax>
+        <descr>
+            Jump to address and place return address in GPR
+            R[rd] = PC + 4; PC = ( R[rs1] + sext(imm) ) & 0xfffffffe
+        </descr>
+    """
+    def fhook(self, ops, vm):
+        target = get_one_op(self.get_nm(), ops)
+        raise Jump(str(target.get_val()))
