d5e064800c09526ba8547a1f7ff1aec51079135d
cindylee0113 <cindylee0113@gmail.com>
2018-11-03 04:06:16 -0400

    Linting

--- tests/MIPS_ASM/test_assemble.py
+++ tests/MIPS_ASM/test_assemble.py
@@ -5,10 +5,9 @@ Test our assembly interpreter.
 
 import sys
 import random
-sys.path.append(".")
+sys.path.append(".") # noqa
 
 import operator as opfunc
-import functools
 
 from unittest import TestCase, main
 
@@ -18,17 +17,19 @@ from assembler.assemble import assemble
 
 # for floating point to binary and back
 import struct
-import codecs
-import binascii
-getBin = lambda x: x > 0 and str(bin(x))[2:] or "-" + str(bin(x))[3:]
- 
+
+getBin = lambda x: x > 0 and str(bin(x))[2:] or "-" + str(bin(x))[3:] # noqa 
+
+
 def f_to_b64(value):
     val = struct.unpack('q', struct.pack('d', value))[0]
     return "0" + getBin(val)
 
+
 def b_to_f64(value):
-    hx = hex(int(value, 2))   
+    hx = hex(int(value, 2))
     return struct.unpack("d", struct.pack("q", int(hx, 16)))[0]
+
 
 NUM_TESTS = 100
 MAX_SHIFT = BITS // 2
@@ -38,11 +39,12 @@ MAX_MUL = 10000  # right now we don't want to overflow!
 MIN_MUL = -10000  # right now we don't want to overflow!
 REGISTER_SIZE = BITS
 
+
 class AssembleTestCase(TestCase):
 
-#####################
-# Two Operand Tests #
-#####################
+    #####################
+    # Two Operand Tests #
+    #####################
 
     def two_op_test(self, operator, instr,
                     low1=MIN_TEST, high1=MAX_TEST,
@@ -54,12 +56,13 @@ class AssembleTestCase(TestCase):
             mips_machine.registers["R8"] = a
             mips_machine.registers["R9"] = b
             mips_machine.base = "hex"
-            assemble("40000 " + instr + " R10, R8, R9", 'mips_asm', mips_machine)
+            assemble("40000 " + instr + " R10, R8, R9",
+                     'mips_asm', mips_machine)
             self.assertEqual(mips_machine.registers["R10"], correct)
 
     def two_op_test_imm(self, operator, instr,
-                    low1=MIN_TEST, high1=MAX_TEST,
-                    low2=MIN_TEST, high2=MAX_TEST):
+                        low1=MIN_TEST, high1=MAX_TEST,
+                        low2=MIN_TEST, high2=MAX_TEST):
         for i in range(0, NUM_TESTS):
             a = random.randint(low1, high1)
             b = random.randint(low2, high2)
@@ -67,7 +70,8 @@ class AssembleTestCase(TestCase):
             correct = operator(a, int(hex(b), 16))
             mips_machine.registers["R9"] = a
             mips_machine.base = "hex"
-            assemble("40000 " + instr + " R10, R9, " + hex_string, 'mips_asm', mips_machine)
+            assemble("40000 " + instr + " R10, R9, " + hex_string,
+                     'mips_asm', mips_machine)
             self.assertEqual(mips_machine.registers["R10"], correct)
 
     def test_add(self):
@@ -84,7 +88,7 @@ class AssembleTestCase(TestCase):
 
     def test_add_imm(self):
         self.two_op_test_imm(opfunc.add, "ADDI")
-        
+
     def test_and_imm(self):
         self.two_op_test_imm(opfunc.and_, "ANDI")
 
@@ -152,17 +156,17 @@ class AssembleTestCase(TestCase):
 
     def test_sll(self):
         self.two_op_test_imm(opfunc.lshift, "SLL",
-                         low1=MIN_MUL, high1=MAX_MUL,
-                         low2=0, high2=MAX_SHIFT)
+                             low1=MIN_MUL, high1=MAX_MUL,
+                             low2=0, high2=MAX_SHIFT)
 
     def test_srl(self):
         self.two_op_test_imm(opfunc.rshift, "SRL",
-                         low1=MIN_MUL, high1=MAX_MUL,
-                         low2=0, high2=MAX_SHIFT)
-        
+                             low1=MIN_MUL, high1=MAX_MUL,
+                             low2=0, high2=MAX_SHIFT)
+
     def two_op_test_float(self, operator, instr,
-                    low1=MIN_TEST, high1=MAX_TEST,
-                    low2=MIN_TEST, high2=MAX_TEST):
+                          low1=MIN_TEST, high1=MAX_TEST,
+                          low2=MIN_TEST, high2=MAX_TEST):
         for i in range(0, NUM_TESTS):
             a = random.uniform(low1, high1)
             b = random.uniform(low2, high2)
@@ -170,29 +174,32 @@ class AssembleTestCase(TestCase):
             mips_machine.registers["F8"] = a
             mips_machine.registers["F10"] = b
             mips_machine.base = "hex"
-            r = assemble("40000 " + instr + " F12, F8, F10", 'mips_asm', mips_machine)
+            assemble("40000 " + instr + " F12, F8, F10",
+                     'mips_asm', mips_machine)
             self.assertEqual(mips_machine.registers["F12"], correct)
 
-    def two_op_test_double_float(self, operator, instr, 
-                    low1=0, high1=MAX_TEST,
-                    low2=0, high2=MAX_TEST):
+    def two_op_test_double_float(self, operator, instr,
+                                 low1=0, high1=MAX_TEST,
+                                 low2=0, high2=MAX_TEST):
         for i in range(0, 1):
             a = random.uniform(low1, high1)
             b = random.uniform(low2, high2)
-            correct = operator(a,b)
+            correct = operator(a, b)
 
             a_binary = f_to_b64(a)
             mips_machine.registers["F8"] = a_binary[:32]
             mips_machine.registers["F9"] = a_binary[32:]
-            
+
             b_binary = f_to_b64(b)
             mips_machine.registers["F10"] = b_binary[:32]
             mips_machine.registers["F11"] = b_binary[32:]
             mips_machine.base = "hex"
 
-            r = assemble("40000 " + instr + " F12, F8, F10", 'mips_asm', mips_machine)
+            assemble("40000 " + instr + " F12, F8, F10",
+                     'mips_asm', mips_machine)
 
-            # the answer from the assembly call will be in the F12, F13 registers
+            # the answer from the assembly call
+            # will be in the F12, F13 registers
 
             first_32 = str(mips_machine.registers["F12"])
             last_32 = str(mips_machine.registers["F13"])
@@ -200,7 +207,6 @@ class AssembleTestCase(TestCase):
             result = b_to_f64(binary_result)
             print(type(mips_machine.registers["F12"]))
             self.assertEqual(result, correct)
-
 
             # h_reg = str(mips_machine.registers["HI"])
             # for i in range(0, 32-len(h_reg)):
@@ -225,10 +231,12 @@ class AssembleTestCase(TestCase):
         self.two_op_test_float(opfunc.sub, "SUB.S")
 
     def test_mults(self):
-        self.two_op_test_float(opfunc.mul, "MULT.S", low1 = 0, high1 = 2 ** 11, low2 = 0, high2 = 2 ** 11)
+        self.two_op_test_float(opfunc.mul, "MULT.S",
+                               low1=0, high1=2 ** 11, low2=0, high2=2 ** 11)
 
     def test_divs(self):
-        self.two_op_test_float(opfunc.truediv, "DIV.S", low1 = 0, high1 = 2 ** 11, low2 = 0, high2 = 2 ** 11)
+        self.two_op_test_float(opfunc.truediv, "DIV.S",
+                               low1=0, high1=2 ** 11, low2=0, high2=2 ** 11)
 
     def test_addd(self):
         self.two_op_test_double_float(opfunc.add, 'ADD.D')
