2e46c844454f2bb3dd4eec5c7862577f40e2c614
Amit Biswas <ab4309@nyu.edu>
2018-11-03 19:12:00 -0400

    Added import of Blt instruction

--- assembler/RISCV/key_words.py
+++ assembler/RISCV/key_words.py
@@ -2,7 +2,7 @@ from .arithmetic import Add, Addi, Sub, Mul, And, Andi
 from .arithmetic import Xor, Xori, Or, Ori, Srl, Sll
 from .arithmetic import Srli, Slli, Slt, Sltu, Slti, Sltiu
 from .arithmetic import Sra, Srai, Div, Divu, Rem, Remu
-from .control_flow import Jr, Jal, Jalr, Beq, Bne
+from .control_flow import Jr, Jal, Jalr, Beq, Bne, Blt
 from .data_mov import Load, Store
 from assembler.tokens import DataType
 
