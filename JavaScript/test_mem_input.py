7157d3dfd7ba6b304b327178c90d21820b8ac058
Cindy Lee <cindylee0113@gmail.com>
2018-12-01 04:08:37 -0500

    Editing JS test to check forhtml mem input

--- JavaScript/test_mem_input.py
+++ JavaScript/test_mem_input.py
@@ -19,21 +19,24 @@ class TestMemory(TestCase):
     def getById(self, id):
         return driver.find_element_by_id(id)
 
+    def get_by_name(self, name):
+        return driver.find_element_by_name(name)
+
     def enterInputs(self, inputBox, value):
         inputBox.send_keys(value)
 
     def set_Mem(self, loc, val):
-        self.getById('subButton').click()
         inputBox = self.getById('memText')
         self.enterInputs(inputBox, hex(loc).upper().split('X')[-1])
         inputVal = self.getById('valueText')
         self.enterInputs(inputVal, hex(val).upper().split('X')[-1])
         self.getById('setMem').click()
 
-    def test_mem(self, low1=0, high1=16):
+    def test_mem(self, low1=0, high1=16, low2=0, high2=100000):
+        self.load_page()
+        self.getById('subButton').click()
         for i in range(0, NUM_TESTS):
-            self.load_page()
-            a = random.randint(low1, high1)
+            a = random.randint(low2, high2)
             b = random.randint(low1, high1)
             message = ""
             try:
@@ -47,7 +50,11 @@ class TestMemory(TestCase):
                 alert.accept()
                 self.assertEqual(message, alert_message)
             except Exception:
+                loc_val = self.get_by_name(hex(a).upper().split('X')[-1])
+                val = loc_val.get_attribute('value')
+                self.assertEqual(val, hex(b).upper().split('X')[-1])
                 self.assertEqual(message, "")
+
         self.close_page()
 
 
