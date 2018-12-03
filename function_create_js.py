6067cd0803f2f0bb41dbc39f6fd8589a5948dd31
Cindy Lee <cindylee0113@gmail.com>
2018-12-03 01:02:36 -0500

    Updating js helper files

--- function_create_js.py
+++ function_create_js.py
@@ -32,6 +32,9 @@ INTEL = 0
 ATT = 1
 MIPS = 2
 
+DEC = 0
+HEX = 1
+
 
 def convert_line_dec_to_hex(code):
     dec_num = re.compile(r'\d+')
@@ -93,7 +96,7 @@ def convert_line_hex_to_fp(code):
         code = code[:start] + str(fp_num) + code[end:]
     return code
 
-def function_directory(func_dict, directory_lst):
+def sample_dir(func_dict, directory_lst, base):
     file_code = ""
     for file_name in func_dict:
         function_code = ""
@@ -114,7 +117,8 @@ def function_directory(func_dict, directory_lst):
                 function_code += "\n\telse{\n"
 
             function_code += "\t\tcode_string += "
-            if count == 0 or count == 1 or count == 3:
+            if ((base == DEC and count == 0 or count == 1) or 
+                (base == HEX and count != 0 and count != 1) or count == 3):
                 function_code += repr(sample_test.read())
             else:
                 sample_conv = ""
@@ -124,7 +128,10 @@ def function_directory(func_dict, directory_lst):
                     elif line.strip()[0] == ";":
                         sample_conv += line
                     else:
-                        sample_conv += convert_line_hex_to_dec(line)
+                        if base == DEC:
+                            sample_conv += convert_line_hex_to_dec(line)
+                        else:
+                            sample_conv += convert_line_dec_to_hex(line)
                 function_code += repr(sample_conv)
             sample_test.close()
             function_code += ";\n\t}"
@@ -135,46 +142,46 @@ def function_directory(func_dict, directory_lst):
     return file_code
 
 
-def function_directory_hex(func_dict, directory_lst):
-    file_code = ""
-    for file_name in func_dict:
-        function_code = ""
-        count = 0
-        function_code += "function " + func_dict[file_name] + "_hex(flavor) {"
-        function_code += "\n\tcode_string = '';"
-        for dire in directory_lst:
-            sample_test = open(dire + file_name, "r")
-            if count == 0:
-                function_code += "\n\tif (flavor == 'intel'){\n"
-            elif count == 1:
-                function_code += "\n\telse if (flavor == 'att'){\n"
-            elif count == 2:
-                function_code += "\n\telse if (flavor == 'mips_asm'){\n"
-            elif count == 3:
-                function_code += "\n\telse if (flavor == 'mips_mml'){\n"
-            else:
-                function_code += "\n\telse{\n"
+# def function_directory_hex(func_dict, directory_lst):
+#     file_code = ""
+#     for file_name in func_dict:
+#         function_code = ""
+#         count = 0
+#         function_code += "function " + func_dict[file_name] + "_hex(flavor) {"
+#         function_code += "\n\tcode_string = '';"
+#         for dire in directory_lst:
+#             sample_test = open(dire + file_name, "r")
+#             if count == 0:
+#                 function_code += "\n\tif (flavor == 'intel'){\n"
+#             elif count == 1:
+#                 function_code += "\n\telse if (flavor == 'att'){\n"
+#             elif count == 2:
+#                 function_code += "\n\telse if (flavor == 'mips_asm'){\n"
+#             elif count == 3:
+#                 function_code += "\n\telse if (flavor == 'mips_mml'){\n"
+#             else:
+#                 function_code += "\n\telse{\n"
 
-            function_code += "\t\tcode_string += "
-            if count == 2 or count == 3 or count == 4:
-                function_code += repr(sample_test.read())
-            else:
-                sample_conv = ""
-                for line in sample_test:
-                    if line.strip() == "":
-                        sample_conv += line
-                    elif line.strip()[0] == ";":
-                        sample_conv += line
-                    else:
-                        sample_conv += convert_line_dec_to_hex(line)
-                function_code += repr(sample_conv)
-            sample_test.close()
-            function_code += ";\n\t}"
-            count += 1
-        function_code += "\n\tdocument.getElementById('id_code')"
-        function_code += ".value = code_string;\n}"
-        file_code += function_code + "\n"
-    return file_code
+#             function_code += "\t\tcode_string += "
+#             if count == 2 or count == 3 or count == 4:
+#                 function_code += repr(sample_test.read())
+#             else:
+#                 sample_conv = ""
+#                 for line in sample_test:
+#                     if line.strip() == "":
+#                         sample_conv += line
+#                     elif line.strip()[0] == ";":
+#                         sample_conv += line
+#                     else:
+#                         sample_conv += convert_line_dec_to_hex(line)
+#                 function_code += repr(sample_conv)
+#             sample_test.close()
+#             function_code += ";\n\t}"
+#             count += 1
+#         function_code += "\n\tdocument.getElementById('id_code')"
+#         function_code += ".value = code_string;\n}"
+#         file_code += function_code + "\n"
+#     return file_code
 
 def function_directory_fp(func_dict, directory_lst):
     file_code = ""
@@ -218,23 +225,23 @@ def function_directory_fp(func_dict, directory_lst):
 
 def create_js_files():
     intel_directory = ["tests/Intel/", "tests/ATT/"]
-    js_file_dec = open("mysite/static/Emu86/helper_functions.js", "w")
-    file_code = function_directory(function_names, intel_directory +
-                                   ["tests/MIPS_ASM/", "tests/MIPS_MML/",
-                                   "tests/RISCV/"])
-    file_code += function_directory(intel_function_names, intel_directory)
+    js_file_dec = open("mysite/static/Emu86/sample_functions.js", "w")
+    file_code = sample_dir(function_names, intel_directory +
+                           ["tests/MIPS_ASM/", "tests/MIPS_MML/",
+                            "tests/RISCV/"], DEC)
+    file_code += sample_dir(intel_function_names, intel_directory, DEC)
     js_file_dec.write(file_code)
     js_file_dec.close()
 
-    js_file_hex = open("mysite/static/Emu86/helper_functions_hex.js", "w")
-    file_code = function_directory_hex(function_names, intel_directory +
-                                       ["tests/MIPS_ASM/", "tests/MIPS_MML/",
-                                       "tests/RISCV/"])
-    file_code += function_directory_hex(intel_function_names, intel_directory)
+    js_file_hex = open("mysite/static/Emu86/sample_functions_hex.js", "w")
+    file_code = sample_dir(function_names, intel_directory +
+                           ["tests/MIPS_ASM/", "tests/MIPS_MML/",
+                            "tests/RISCV/"], HEX)
+    file_code += sample_dir(intel_function_names, intel_directory, HEX)
     js_file_hex.write(file_code)
     js_file_hex.close()
 
-    js_file_fp = open("mysite/static/Emu86/helper_functions_fp.js", "w")
+    js_file_fp = open("mysite/static/Emu86/sample_functions_fp.js", "w")
     file_code = function_directory_fp(fp_function_names, ["tests/MIPS_ASM/"])
     # file_code += function_directory_fp(intel_function_names, intel_directory)
     js_file_fp.write(file_code)
