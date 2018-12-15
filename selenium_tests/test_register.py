8b1a22bafa74073a93f124624b85a2b50209da88
Cindy Lee <cindylee0113@gmail.com>
2018-12-14 20:58:39 -0500

    Adding register click test for MIPS hex

--- selenium_tests/test_register.py
+++ selenium_tests/test_register.py
@@ -20,7 +20,24 @@ class TestRegisters(TestCase):
     def get_by_name(self, name):
         return self.driver.find_element_by_name(name)
 
-    def binary_val(self, value):
+    def binary_val_hex(self, value):
+        '''
+        Determine if the value is negative or positive
+        After converting the value to decimal without the negative sign to hex
+        Call the binary conversion on the converted decimal number
+        '''
+        neg = False
+        if value[0] == "-":
+            neg = True
+            value = int(value[1:], 16)
+        else:
+            value = int(value, 16)
+        if neg:
+            value *= -1
+        return self.binary_val_dec(value)
+
+    def binary_val_dec(self, value):
+        value = int(value)
         if value >= 0:
             return bin(value).split('b')[-1]
         else:
@@ -42,34 +59,55 @@ class TestRegisters(TestCase):
             neg_b = (32 - len(pos_b)) * '1' + neg_b
             return neg_b
 
-    def test_reg(self):
+    def reg_test(self, lang, reg_lst):
         self.driver = webdriver.Chrome()
         self.load_page()
         lang_flag = '//*[@id="content-main"]/h5/form/select/option['
-        lang_flag += str(1) + ']'
+        lang_flag += str(lang) + ']'
+        # set the number system 
+        num_sys = "dec"
+        if lang == 3:
+            num_sys = "hex"
         self.get_by_x_path(lang_flag).click()
         self.get_by_id('subButton').click()
         option_path = '//*[@id="sample"]/option'
         self.get_by_x_path('//*[@id="sample"]').click()
-        registers = ['EAX', 'EBX', 'ECX', 'EDX']
-        # for sample_opt in range(8, 17):
+        # for each sample, run the register click test
         for sample_opt in range(2, 17):
+            # make sure to reinitialize before running
             self.get_by_id('clear-button').click()
             opt_num_path = option_path + '[' + str(sample_opt) + ']'
             self.get_by_x_path(opt_num_path).click()
             self.get_by_id('run-button').click()
-            for reg in registers:
+            # go through each register in the list and click!
+            for reg in reg_lst:
                 reg_intel = self.get_by_name(reg)
-                reg_val = int(reg_intel.get_attribute('value'))
+                reg_val = reg_intel.get_attribute('value')
                 reg_intel.click()
                 message = 'Binary value of ' + reg + ': '
-                message += self.binary_val(reg_val)
+                if num_sys == "dec":
+                    message += self.binary_val_dec(reg_val)
+                else:
+                    message += self.binary_val_hex(reg_val)
                 alert = self.driver.switch_to.alert
                 alert_message = alert.text
                 alert.accept()
+                # check to see if the binary message is the same as the 
+                # alert message
                 self.assertEqual(message, alert_message)
         self.close_page()
 
+    def test_reg_conversion(self):
+        # testing register clicks for Intel in decimal
+        self.reg_test(1, ['EAX', 'EBX', 'ECX', 'EDX'])
+
+        # create the register list for mips
+        mips_reg = []
+        for i in range(1, 18):
+            mips_reg.append("R" + str(i))
+
+        # testing register clicks for MIPS in hex
+        self.reg_test(3, mips_reg)
 
 if __name__ == '__main__':
     main()
