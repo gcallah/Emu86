d10dc3933ff527bd6f1483d1f4552713f736f8c7
Nikhil Vaidyamath <nuv200@nyu.edu>
2019-01-14 23:45:04 -0800

    created instruction for fabs and tested

--- assembler/Intel/fp_arithmetic.py
+++ assembler/Intel/fp_arithmetic.py
@@ -10,7 +10,7 @@ from assembler.ops_check import one_op_arith
 from assembler.virtual_machine import intel_machine, STACK_TOP, STACK_BOTTOM
 from .arithmetic import checkflag
 from ctypes import *
-from fp_conversions import anyfloat
+from .fp_conversions import anyfloat
 
 def dec_convert(val):
     while val > 1:
@@ -52,46 +52,7 @@ class FSub(Instruction):
 
 
 class FMul(Instruction):
-    def multiplyMantissas(val1, val2):
-        val1 = '1' + val1
-        val2 = '1' + val2
-        return(int(val1, 2)*int(val2, 2))
 
-    def convertIEEE(val):
-        binary = convert_float_binary(val)
-        binary = binary[1:]
-        found = False
-        posVal = 0
-        posDecimal = 0
-        for i in range(len(binary)):
-            if binary[i] == '1' and not found:
-                posVal = i
-                found = True
-            if binary[i] == '.':
-                posDecimal = i
-        expo = posDecimal - posVal - 1
-        Mantissa = binary[posVal+1:posDecimal] + binary[posDecimal+1:]
-        return (expo, Mantissa)
-
-    def multiply(val1, val2):
-        convert1 = FMul.convertIEEE(val1)
-        convert2 = FMul.convertIEEE(val2)
-        productMantisa = FMul.multiplyMantissas(convert1[1], convert2[1])
-        bitresult = str(bin(productMantisa))[1:]
-        print(bitresult.lstrip("0b"))
-        while len(bitresult) < 23:
-            bitresult += '0'
-        exponentResult = (convert1[0] + convert2[0]) + 127
-        returnBin = None
-        expoFinalResult = bin(exponentResult).lstrip("0b")
-
-        while len(expoFinalResult) != 8:
-            expoFinalResult += '0'
-        if val1 < 0 or val2 < 0:
-            returnBin = '1'+expoFinalResult+bitresult.lstrip("0b")
-        else:
-            returnBin = '0'+expoFinalResult+bitresult.lstrip("0b")
-        return returnBin
 
     def fhook(self, ops, vm):
         two_op_arith(ops, vm, self.name, opfunc.mul)
@@ -103,6 +64,21 @@ class FDec(Instruction):
         ops[0].set_val(ops[0].get_val() - 1)
         vm.changes.add(ops[0].get_nm())
 
+class FAbs(Instruction):
+        """
+        sets bit  of floating-point register (FPR) FRB to 0 and place the results into FPR FRT
+            <instr>
+                 test_fabs
+            </instr>
+            <syntax>
+                fabs FRT, FRB
+            </syntax>
+        """
+        def fhook(self, ops, vm):
+            intel_machine.registers["FRB"]
+            IEEE = anyfloat.from_float(intel_machine.registers["FRB"])
+            IEEE.changeSign()
+            intel_machine.registers["FRT"]=float(IEEE)
 
 class FDiv(Instruction):
     def fhook(self, ops, vm):
