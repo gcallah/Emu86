c1fffac3b127439bb142681e778ac24a724542be
Susanna Xu <sx563@nyu.edu>
2019-04-15 01:38:54 -0400

    add new key words

--- assembler/WASM/key_words.py
+++ assembler/WASM/key_words.py
@@ -1,4 +1,7 @@
 from .arithmetic import Add, Sub, Mul, Div_S, Div_U
+from .arithmetic import Rem_S, Rem_U, And, Or, Xor
+from .arithmetic import Shl, Shr_S, Shr_U, Rotl, Rotr
+from .arithmetic import Clz, Ctz, Popcnt, Eqz
 from .data_mov import Global_mov, Local_mov, Store_global, Store_local
 from assembler.tokens import DataType
 
@@ -6,12 +9,28 @@ key_words = {
     # data types
     'i32': DataType('i32'),
 
-    # arithmetic
+    # arithmetic 
     'i32.add': Add('.add'),
     'i32.sub': Sub('.sub'),
     'i32.mul': Mul('.mul'),
     'i32.div_s': Div_S('.div_s'),
     'i32.div_u': Div_U('.div_u'),
+    'i32.rem_s': Rem_S('.rem_s'),
+    'i32.rem_u': Rem_U('.rem_u'),
+    'i32.and': And('.and'),
+    'i32.or': Or('.or'),
+    'i32.xor': Xor('.xor'),
+    'i32.shl': Shl('.shl'),
+    'i32.shr_s': Shr_S('.shr_s'),
+    'i32.shr_u': Shr_U('.shr_u'),
+    'i32.rotl': Rotl('.rotl'),
+    'i32.rotr': Rotr('.rotr'),
+    'i32.clz': Clz('.clz'),
+    'i32.ctz': Ctz('.ctz'),
+    'i32.popcnt': Popcnt('.popcnt'),
+    'i32.eqz': Eqz('.eqz'),
+
+    # data movement
     'global.get': Global_mov('global.get'),
     'local.get': Local_mov('local.get'),
     'global': Store_global('global'),
