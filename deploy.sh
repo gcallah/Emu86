9c51c5c42dab98bb152171efa90aec9b08cea903
gcallah <gcallah@mac.com>
2019-04-19 15:35:14 -0400

    Trying to put travis things back so build works.

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@
 exit 0
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/emu_rsa # Allow read access to the private key
-ssh-add ~/.ssh/emu_rsa # Add the private key to SSH
+chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
+ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
 ssh gcallah@ssh.pythonanywhere.com << EOF
