a28b80c313ff81cc5c518a1ab503347061e0a61a
gcallah <gcallah@mac.com>
2019-04-24 11:54:49 -0400

    Fixed deploy script to not just exit(0)

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # This shell file deploys a new version to our server.
 
-# since the server setup is not complete, let's just exit with success:
-exit 0
-
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
