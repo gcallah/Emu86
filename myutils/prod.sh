f20d270012113b41a975063373dcffee5e7b0b26
gcallah <gcallah@mac.com>
2019-04-24 12:42:51 -0400

    Added prints to prod.sh to see what is happening on PA.

--- myutils/prod.sh
+++ myutils/prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # for the dev server: fetches new code and restarts the server.
 
+echo "Getting new files from GitHub."
 git pull origin master
+echo "Rebooting serber."
 touch /var/www/www_emu86_org_wsgi.py
