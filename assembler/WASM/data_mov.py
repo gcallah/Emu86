ad9288b168f90166e62db4ca714d8325200c623d
Cindy Lee <cindylee0113@gmail.com>
2019-04-26 00:59:44 -0400

    Adding wasm movement instructions for setting global variables and adding constant values to stACK

--- assembler/WASM/data_mov.py
+++ assembler/WASM/data_mov.py
@@ -1,8 +1,8 @@
 from assembler.errors import check_num_args, InvalidArgument
-from assembler.tokens import Instruction, Symbol
+from assembler.tokens import Instruction, Symbol, IntegerTok
 
 
-class Global_mov(Instruction):
+class Global_get(Instruction):
     """
         <instr>
              global.get
@@ -27,7 +27,32 @@ class Global_mov(Instruction):
             raise InvalidArgument(ops[0].get_nm())
 
 
-class Local_mov(Instruction):
+class Global_set(Instruction):
+    """
+        <instr>
+             global.set
+        </instr>
+        <syntax>
+            global.set var
+        </syntax>
+        <descr>
+            Copies the value of op1 onto the stack
+        </descr>
+    """
+    def fhook(self, ops, vm):
+        check_num_args(self.get_nm(), ops, 1)
+        if isinstance(ops[0], Symbol):
+            try:
+                vm.dec_sp()
+                stack_loc = hex(vm.get_sp() + 1).split('x')[-1].upper()
+                vm.globals[ops[0].get_nm()].set_val(vm.stack[stack_loc])
+            except Exception:
+                raise InvalidArgument(ops[0].get_nm())
+        else:
+            raise InvalidArgument(ops[0].get_nm())
+
+
+class Local_get(Instruction):
     """
         <instr>
              local.get
@@ -45,7 +70,32 @@ class Local_mov(Instruction):
             try:
                 vm.dec_sp()
                 stack_loc = hex(vm.get_sp() + 1).split('x')[-1].upper()
-                vm.stack[stack_loc] = vm.local[ops[0].get_nm()].get_val()
+                vm.stack[stack_loc] = vm.locals[ops[0].get_nm()].get_val()
+            except Exception:
+                raise InvalidArgument(ops[0].get_nm())
+        else:
+            raise InvalidArgument(ops[0].get_nm())
+
+
+class Local_set(Instruction):
+    """
+        <instr>
+             local.set
+        </instr>
+        <syntax>
+            local.set var
+        </syntax>
+        <descr>
+            Copies the value of op1 onto the stack
+        </descr>
+    """
+    def fhook(self, ops, vm):
+        check_num_args(self.get_nm(), ops, 1)
+        if isinstance(ops[0], Symbol):
+            try:
+                vm.dec_sp()
+                stack_loc = hex(vm.get_sp() + 1).split('x')[-1].upper()
+                vm.locals[ops[0].get_nm()].set_val(vm.stack[stack_loc])
             except Exception:
                 raise InvalidArgument(ops[0].get_nm())
         else:
@@ -92,6 +142,31 @@ class Store_local(Instruction):
         if isinstance(ops[0], Symbol):
             try:
                 vm.locals[ops[0].get_nm()] = ops[0]
+            except Exception:
+                raise InvalidArgument(ops[0].get_nm())
+        else:
+            raise InvalidArgument(ops[0].get_nm())
+
+
+class Store_const(Instruction):
+    """
+        <instr>
+             i32.const
+        </instr>
+        <syntax>
+            i32.const val
+        </syntax>
+        <descr>
+            Store a constant value onto the stack
+        </descr>
+    """
+    def fhook(self, ops, vm):
+        check_num_args(self.get_nm(), ops, 1)
+        if isinstance(ops[0], IntegerTok):
+            try:
+                vm.dec_sp()
+                stack_loc = hex(vm.get_sp() + 1).split('x')[-1].upper()
+                vm.stack[stack_loc] = vm.local[ops[0].get_val()]
             except Exception:
                 raise InvalidArgument(ops[0].get_nm())
         else:
