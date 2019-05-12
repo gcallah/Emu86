8398392ed876f23f8f122bb759138ed0a7b7086e
kin393 <kmr591@nyu.edu>
2019-05-12 16:44:05 -0400

    Changing the sample program for fp operations

--- mysite/static/Emu86/sample_functions_fp.js
+++ mysite/static/Emu86/sample_functions_fp.js
@@ -1,7 +1,7 @@
 function area_fp(flavor) {
 	let codeString = '';
 	if (flavor === 'intel'){
-		codeString += '; Declare length and width\n.data\n    long DW 0x40600000\n    wide DW 0x1B\n\n; Calculate area of rectangle\n.text\n    mov F7, [long]\n    imul F6, [wide]\n';
+		codeString += '; Declare length and width\n.data\n    long DW 0x40600000\n    wide DW 0x1B\n\n; Calculate area of rectangle\n.text\n    FLD F7, [long]\n    FMUL F6, [wide]\n';
 	}
 	else if (flavor === 'mips_asm'){
 	codeString += '; Declare length and width\n.data\n\tlong: .float 12.2\n\twide: .float 12.5\n\n; Calc area of rect\n.text\n\t0x40000 LWC F8, 0(F28)\n    0x40004 LWC F10, 4(F28)\n    0x40008 MULT.S F12, F8, F10';
@@ -11,7 +11,7 @@ function area_fp(flavor) {
 function data_fp(flavor) {
 	let codeString = '';
 	if (flavor === 'intel'){
-		codeString += '; Declare arrays\n.data\n    x DB 0x3f99999a, 0x2, 0x40600000, 0x4, 0x5\n    y DW 0x2, 0x36, 0x3fb33333, 0x8\n    z DD 0xA DUP (0x32)\n\n; Storing values into memory using register arithmetic\n.text\n    mov F7, 0x6\n    mov [F7], [x+0x2]\n    mov [F7+0x2], [y+0x3]\n    mov [F6], [z]\n    mov [F7-0x5], [y+0x2]\n    mov [-0x5+F7], [y+0x2]';
+		codeString += '; Declare arrays\n.data\n    x DB 0x3f99999a, 0x2, 0x40600000, 0x4, 0x5\n    y DW 0x2, 0x36, 0x3fb33333, 0x8\n    z DD 0xA DUP (0x32)\n\n; Storing values into memory using register arithmetic\n.text\n    FLD F7, 0x6\n    FLD [F7], [x+0x2]\n    FLD [F7+0x2], [y+0x3]\n    FLD [F6], [z]\n    FLD [F7-0x5], [y+0x2]\n    FLD [-0x5+F7], [y+0x2]';
 	}
 	else if (flavor === 'mips_asm'){
 	codeString += '; First comes the data section, where we declare some names.\n.data\n    x: .float 8.0\n    y: .float 10.5\n    z: .double 20.555\n\n; Next is the .text section, where we use them:\n.text\n    0x400000 LWC F8, 0(F28)\n    0x400004 LWC F10, 4(F28)\n    0x400008 LDC F12, 8(F28)\n';
