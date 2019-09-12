0ede4815118cb8b3fcf9bf2a122475fcb94aaeb1
gcallah <gcallah@mac.com>
2019-09-12 14:34:41 -0400

    Updated django version and PA reboot script.

--- myutils/prod.sh
+++ myutils/prod.sh
@@ -3,5 +3,9 @@
 
 echo "Getting new files from GitHub."
 git pull origin master
-echo "Rebooting serber."
+# activate our virtual env:
+source /home/gcallah/.virtualenvs/django2/bin/activate
+# install all of our packages:
+pip install -r docker/requirements.txt
+echo "Rebooting server."
 touch /var/www/www_emu86_org_wsgi.py
