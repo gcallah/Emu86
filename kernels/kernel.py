a2383c944f040cca4281da705ae2c9e3d215bcc4
Cindy Lee <cindylee0113@gmail.com>
2019-09-18 00:01:45 -0400

    Setting the base and flavor of the intel machine

--- kernels/kernel.py
+++ kernels/kernel.py
@@ -2,6 +2,7 @@ from ipykernel.kernelbase import Kernel
 from assembler.virtual_machine import IntelMachine
 from assembler.assemble import assemble
 
+
 class IntelKernel(Kernel):
     implementation = 'intel_kernel'
     implementation_version = '1.0'
@@ -13,19 +14,19 @@ class IntelKernel(Kernel):
         'file_extension': '.txt',
     }
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
-            intel_machine = IntelMachine()
-            (last_instr, error, bit_code) = assemble(code, intel_machine)
-            if not error:
-                result = {'name': 'return_value', 'text': intel_machine.registers}
-                self.send_response(self.iopub_socket, 'result', result)
-            else:
-                self.send_response(self.iopub_socket, 'result', "error")
-            
-
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        intel_machine = IntelMachine()
+        intel_machine.base = 'dec'
+        intel_machine.flavor = 'intel'
+        (last_instr, error, bit_code) = assemble(code, intel_machine)
+        if not error:
+            result = {'name': 'return_value', 'text': intel_machine.registers}
+            self.send_response(self.iopub_socket, 'result', result)
+        else:
+            self.send_response(self.iopub_socket, 'result', "error")
         return {'status': 'ok',
                 'execution_count': self.execution_count,
                 'payload': [],
-                'user_expressions': {},
-               }
+                'user_expressions': {}
+                }
