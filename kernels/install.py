82941107b217fe9deb0403740182fa26647b8337
Susanna Xu <sx563@nyu.edu>
2019-09-17 23:22:56 -0400

    add install and uninstall kernel functionalities

--- kernels/install.py
+++ kernels/install.py
@@ -1,4 +1,21 @@
-import json 
-import sys
-import os
-import argparse 
+from jupyter_client.kernelspecapp  import InstallKernelSpec, RemoveKernelSpec
+
+
+class InstallIntelKernel(InstallKernelSpec):
+    version = 1.0
+    kernel_name = "intel_kernel"
+    description = "Install Intel kernel"
+    
+    def parse_command_line(self, argv):
+        super(InstallKernelSpec, self).parse_command_line(argv)
+
+
+class UninstallIntelKernel(RemoveKernelSpec):
+    version = 1.0
+    kernel_name = "intel_kernel"
+    description = "Uninstall Intel kernel"
+
+    def parse_command_line(self, argv):
+        super(RemoveKernelSpec, self).parse_command_line(argv)
+
+   
