6a4b0c95a364771ae9a9ef581f7767ce8d27af43
Susanna Xu <sx563@nyu.edu>
2019-10-11 18:16:09 -0400

    add setup file for pypi releases

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='emu86',
+    version='0.0.2',
+    packages=['kernels', 'kernels/att', 'kernels/intel', 'kernels/riscv',
+              'kernels/mips_asm', 'kernels/mips_mml', 
+              'assembler', 'assembler/Intel', 'assembler/MIPS',
+              'assembler/RISCV', 'assembler/WASM'],
+)
