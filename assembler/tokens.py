d4db1bf0fca35dce1604736fdc0161e86c8b77fe
Cindy Lee <cindylee0113@gmail.com>
2019-10-15 16:55:51 -0400

    pypi release v0.0.5 getting rid of the print statements in parse and tokens

--- assembler/tokens.py
+++ assembler/tokens.py
@@ -294,7 +294,6 @@ class RegAddress(Address):
 
     def get_mem_addr(self):
         # right now, memory addresses are strings. eeh!
-        print(self.multiplier, self.displacement)
         address = hex(int(self.regs[self.name]) *
                       self.multiplier).split('x')[-1].upper()
         disp = 0
@@ -324,8 +323,6 @@ class RegAddress(Address):
     def set_val(self, val):
         mem_addr = self.get_mem_addr()
         self.mem[str(mem_addr)] = val
-        print(mem_addr)
-        print('YO', self.mem)
 
 
 class Register(Location):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='emu86',
-    version='0.0.4',
+    version='0.0.5',
     packages=['kernels', 'kernels/att', 'kernels/intel', 'kernels/riscv',
               'kernels/mips_asm', 'kernels/mips_mml', 
               'assembler', 'assembler/Intel', 'assembler/MIPS',
