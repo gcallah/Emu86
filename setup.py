db9bfdec08980544c8876d4af9173ad195575e36
Cindy Lee <cindylee0113@gmail.com>
2019-10-17 04:42:29 -0400

    pypi release v0.0.6 Rendering changes in vm as a table of register and memory changes

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='emu86',
-    version='0.0.5',
+    version='0.0.6',
     packages=['kernels', 'kernels/att', 'kernels/intel', 'kernels/riscv',
-              'kernels/mips_asm', 'kernels/mips_mml', 
+              'kernels/mips_asm', 'kernels/mips_mml',
               'assembler', 'assembler/Intel', 'assembler/MIPS',
               'assembler/RISCV', 'assembler/WASM'],
 )
