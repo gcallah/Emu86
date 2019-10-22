2c6aa8691bf8528e4001d7b11564597e38279f6b
Neel Chauhan <nc1682@nyu.edu>
2019-10-22 13:30:38 -0400

    Modularize docker config

--- dev_cont.sh
+++ dev_cont.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export HOST_PORT="8000"
 export REPO=emu86
+export PROJECT=Emu86
 if [ $1 ]
 then
     HOST_PORT=$1
@@ -9,4 +10,4 @@ fi
 echo "Going to remove any lingering indra-dev container."
 docker rm emu86-dev 2> /dev/null || true
 echo "Now running docker to spin up the container."
-docker run -it -p $HOST_PORT:8000 -v $PWD:/home/IndrasNet gcallah/$REPO-dev bash
+docker run -it -p $HOST_PORT:8000 -v $PWD:/home/$PROJECT gcallah/$REPO-dev bash
