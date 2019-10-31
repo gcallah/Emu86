88612996804f49ff24b44b8ad0ca2e151ceec897
NathanConroy <nathanconroydev@gmail.com>
2019-10-31 15:57:34 -0500

    Change MIDDLEWARE_CLASSES to MIDDLEWARE. Remove django.contrib.auth.middleware.SessionAuthenticationMiddleware from MIDDLEWARE. Dev container script: fixed typo, removed parameter.

--- dev_cont.sh
+++ dev_cont.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 export HOST_PORT="8000"
 export REPO=emu86
-export PROJECT=Emu86
 if [ $1 ]
 then
     HOST_PORT=$1
 fi
 
-echo "Going to remove any lingering indra-dev container."
+echo "Going to remove any lingering emu86-dev container."
 docker rm emu86-dev 2> /dev/null || true
 echo "Now running docker to spin up the container."
-docker run -it -p $HOST_PORT:8000 -v $PWD:/home/$PROJECT gcallah/$REPO-dev bash
+docker run -it -p $HOST_PORT:8000 -v $PWD:/home/Emu86 gcallah/$REPO-dev bash
