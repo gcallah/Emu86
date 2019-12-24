84bddd9c92d945d31334c6a880e47c0a87f563ed
Susanna Xu <sx563@nyu.edu>
2019-12-23 19:42:33 -0500

    pypi release v0.0.9.4 update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('Emu86 Kernel PYPI Description.md', 'r') as f:
 
 setup(
     name='emu86',
-    version='0.0.9.3',
+    version='0.0.9.4',
     packages=['kernels', 'kernels/att', 'kernels/intel', 'kernels/riscv',
               'kernels/mips_asm', 'kernels/mips_mml',
               'assembler', 'assembler/Intel', 'assembler/MIPS',
