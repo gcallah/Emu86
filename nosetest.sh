528e266d81f61d879192dad9caf8cc20f672301f
Big Chungus <bigchungus@128-122-203-235.dynapool.nyu.edu>
2020-02-19 20:01:56 -0500

    Created the nosetest file, though it doesn't seem to work properly

---
+++ nosetest.sh
@@ -0,0 +1,11 @@
+export user_type="test"
+export test_dir="tests"
+export ignores="(scheduler|sandpile|fashion|bigbox)"
+
+if [ -z "$1" ]; then
+	export capture=""
+else
+	export capture="--nocapture"
+fi
+
+nosetests --ignore-files=$ignores --exe --verbose --with-coverage --cover-package=models $capture
