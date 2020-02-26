7777f919085661750dc7ad35d5b4608f888b6a45
glitchmanbeta <almawaldi@hotmail.com>
2020-02-26 00:10:00 -0500

    Added utils to ignore

--- nosetest.sh
+++ nosetest.sh
@@ -1,6 +1,6 @@
 export user_type="test"
 export test_dir="tests"
-export ignores="(scheduler|sandpile|fashion|bigbox)"
+export ignores="(scheduler|sandpile|fashion|bigbox|utils)"
 
 if [ -z "$1" ]; then
 	export capture=""
