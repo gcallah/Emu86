d203ec9d40fa85bb9873b4a8c0c6e82d4f263313
glitchmanbeta <almawaldi@hotmail.com>
2020-05-08 00:19:24 -0400

    Fixed RISCV stack and a MIPS keyerror

--- assembler/MIPS/data_mov.py
+++ assembler/MIPS/data_mov.py
@@ -24,7 +24,7 @@ class Load(Instruction):
             if isinstance(ops[1], RegAddress):
                 stack = int(ops[1].get_mem_addr(line_num), 16)
                 if(vm.check_stack(stack)):
-                    ops[0].set_val(vm.stack[stack], line_num)
+                    ops[0].set_val(vm.stack[hex(stack).split('x')[-1].upper()], line_num)
                 else:
                     ops[0].set_val(ops[1].get_val(line_num), line_num)
                 vm.changes.add(ops[0].get_nm())
