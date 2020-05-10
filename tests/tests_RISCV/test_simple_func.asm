22c7909f8d61d9a329caf459e8b871633ddec588
glitchmanbeta <almawaldi@hotmail.com>
2020-05-10 01:24:21 -0400

    Minor error in RISCV/test_simple_func

--- tests/tests_RISCV/test_simple_func.asm
+++ tests/tests_RISCV/test_simple_func.asm
@@ -4,7 +4,7 @@
 ; Stores the result in X10 (6C4A4)
 main:     0x00 addi X12, X0, 0x299
           0x04 addi X12, X12, 0x1
-          0x08 jal 0x4
+          0x08 jal X1, 0x4
           0x0C syscall
-someFunc: 0x10 mult X10, X12, X12
-          0x18 jr R31
+someFunc: 0x10 mul X10, X12, X12
+          0x14 jr X1
