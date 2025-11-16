ba7ac839199a43b96dafad11b2fa6070c2d03aec
stevenkang312 <84217293+stevenkang312@users.noreply.github.com>
2025-11-16 12:44:50 -0500

    push PA on GitActions

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "SSHing to PythonAnywhere."
-ssh gcallah@ssh.pythonanywhere.com << EOF
-    cd /home/gcallah/Emu86
-    /home/gcallah/Emu86/myutils/prod.sh
+ssh emu86@ssh.pythonanywhere.com << EOF
+    cd /home/emu86/Emu86
+    /home/emu86/Emu86/myutils/prod.sh
 EOF
 
--- myutils/prod.sh
+++ myutils/prod.sh
@@ -4,8 +4,8 @@
 echo "Getting new files from GitHub."
 git pull origin master
 # activate our virtual env:
-source /home/gcallah/.virtualenvs/django2/bin/activate
+source /home/emu86/.virtualenvs/emu86/bin/activate
 # install all of our packages:
-make prod
+make prod_env
 echo "Rebooting server."
 touch /var/www/www_emu86_org_wsgi.py
