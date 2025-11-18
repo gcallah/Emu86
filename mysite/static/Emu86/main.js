09e3b376853fc638d01f4c88ade076e4024cc3b9
Jerry Wu <jerrywu.works@gmail.com>
2025-11-18 18:36:21 -0500

    added highlight back to step function

--- mysite/static/Emu86/main.js
+++ mysite/static/Emu86/main.js
@@ -213,22 +213,34 @@ function highlightCode(){
     if (lastInstr.indexOf(": Exiting program") !== -1){
         lastInstr = lastInstr.substring(0, lastInstr.indexOf(": Exiting program"));
     }
-    if (instr && lastInstr) {
-        const input = document.getElementById("id_code");
-        const codeArray = input.value.split("\n");
-        let startIndex = 0;
-        let lineLength = 0;
-        for (let index = 0; index < codeArray.length; index++) {
-            lineLength = codeArray[index].length;
-            if (index === line_num - 1){
-                break;
+    if (instr && lastInstr && line_num) {
+        // codeMirror
+        if (window.editor) {
+            // clear prev highlights
+            for (let i = 0; i < window.editor.lineCount(); i++) {
+                window.editor.removeLineClass(i, 'background', 'line-highlight');
             }
-            startIndex += codeArray[index].length + 1;
-            // the extra + 1 is to take the new line character into account
+
+            window.editor.addLineClass(line_num - 1, 'background', 'line-highlight');
+
+            window.editor.scrollIntoView({line: line_num - 1, ch: 0}, 100);
+        } else {
+            const input = document.getElementById("id_code");
+            const codeArray = input.value.split("\n");
+            let startIndex = 0;
+            let lineLength = 0;
+            for (let index = 0; index < codeArray.length; index++) {
+                lineLength = codeArray[index].length;
+                if (index === line_num - 1){
+                    break;
+                }
+                startIndex += codeArray[index].length + 1;
+                // the extra + 1 is to take the new line character into account
+            }
+            input.focus();
+            startIndex += lineLength - lastInstr.length + 1;
+            input.setSelectionRange(startIndex, startIndex + lastInstr.length);
         }
-        input.focus();
-        startIndex += lineLength - lastInstr.length + 1;
-        input.setSelectionRange(startIndex, startIndex + lastInstr.length);
     }
 }
 
